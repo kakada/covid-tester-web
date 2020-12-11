8f49b09456ba55f5a5e291cf43e9628ee0ad9aed
kakada <kakada.chheang@gmail.com>
2020-12-11 22:44:53 +0700

    Download testers to .csv

--- app/controllers/testers_controller.rb
+++ app/controllers/testers_controller.rb
@@ -1,5 +1,10 @@
 class TestersController < ApplicationController
   def index
     @testers = Tester.all
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @testers.to_csv, filename: "testers-#{Time.now}.csv" }
+    end
   end
 end
--- app/models/tester.rb
+++ app/models/tester.rb
@@ -1,2 +1,15 @@
+require 'csv'
+
 class Tester < ApplicationRecord
+  def self.to_csv
+    attributes = %w{phone_number age gender lab_id visited_at}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |tester|
+        csv << [tester.phone_number, tester.age, tester.gender, tester.lab_id, tester.visited_at.in_time_zone('Asia/Bangkok')]
+      end
+    end
+  end
 end
